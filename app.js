a3e267c363936ab5987e102d268e1849fc3dfaf5
Jon Schubbe <jonshoob@gmail.com>
2017-02-12 13:04:45 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,18 @@
+
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+
+
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname+'/client/index.html');
+});
+
+app.use('/client',express.static(__dirname+'/client'));
+
+app.use('/client/scripts', express.static(__dirname + '/node_modules/pixi.js/dist/'));
+
+serv.listen(2000);
+console.log("Server started.");
+
